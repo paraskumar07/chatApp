d148756c82968d92a7f3ab4b2734d1f675a5ea47
parasMulodia <parasmulodia@gmail.com>
2025-07-09 12:16:16 +0000

    fix: serve frontend & increase payload size

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,10 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -35,6 +39,13 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+// });
+
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
